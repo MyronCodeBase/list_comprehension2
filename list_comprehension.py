33a36050af842326618498dfb25f91b780f88399
Rahul_patel <54207929+the-rahulpatel@users.noreply.github.com>
2020-10-01 09:14:03 +1000

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -4,8 +4,7 @@ import random
 def list_comprehension():
     a = random.sample(range(10), 9)
     b = random.sample(range(10), 6)
-    new_list = [i for i in a if i in b]
-    new_list.sort()
+    new_list = [i for i in set(a) & set(b)]
     print(new_list)
 
 list_comprehension()
